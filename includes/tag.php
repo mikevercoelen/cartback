bd8860bcf9fb547cbd3e79ecb548d2cae32311e9
Mike Vercoelen <mike.vercoelen@gmail.com>
2021-01-06 15:35:13 +0800

    Clean up

--- includes/tag.php
+++ includes/tag.php
@@ -2,11 +2,15 @@
 
 defined('ABSPATH') || exit;
 
+use \DrewM\MailChimp\MailChimp;
+
 $CB_SETTINGS = get_option('cartback_setting');
 $MC_API_KEY = $CB_SETTINGS['api_key'];
 $MC_LIST_ID = $CB_SETTINGS['list_id'];
 $MC_TAG = $CB_SETTINGS['mailchimp_tag'];
 $MC_TAG_HARDCODED = '_cartback';
+
+$MailChimp = new MailChimp($MC_API_KEY);
 
 function cartback_handle_tag_mailchimp($email) {
   global $MC_TAG;
@@ -31,6 +35,60 @@ function cartback_handle_tag_mailchimp($email) {
       'status' => $MC_TAG_STATUS_ACTIVE
     ]
   ));
+}
+
+$MC_MEMBER_STATUS_NOT_FOUND = '404';
+$MC_MEMBER_STATUS_UNSUBSCRIBED = 'unsubscribed';
+$MC_TAG_STATUS_ACTIVE = 'active';
+$MC_TAG_STATUS_INACTIVE = 'inactive';
+
+function cartback_get_email($request) {
+  if (is_user_logged_in()) {
+    $current_user = wp_get_current_user();
+    return $current_user->user_email;
+  }
+
+  $body = json_decode($request->get_body());
+  return $body->email;
+}
+
+function cartback_mc_is_subscribed($subscriber_hash) {
+  global $MC_MEMBER_STATUS_NOT_FOUND;
+
+  $member = cartback_mc_get_member($subscriber_hash);
+  $member_status = $member['status'];
+
+  if ($member_status == $MC_MEMBER_STATUS_NOT_FOUND) {
+    return false;
+  }
+
+  return true;
+}
+
+function cartback_mc_add_tags($subscriber_hash, $tags) {
+  global $MailChimp;
+  global $MC_LIST_ID;
+
+  return $MailChimp->post("lists/$MC_LIST_ID/members/$subscriber_hash/tags", [
+    'tags' => $tags
+  ]);
+}
+
+function cartback_mc_subscribe($email, $status) {
+  global $MailChimp;
+  global $MC_LIST_ID;
+
+  return $MailChimp->post("lists/" . $MC_LIST_ID . "/members", [
+    'email_address' => $email,
+    'status' => $status
+  ]);
+}
+
+function cartback_mc_get_member($subscriber_hash) {
+  global $MailChimp;
+  global $MC_LIST_ID;
+
+  return $MailChimp->get("lists/$MC_LIST_ID/members/$subscriber_hash");
 }
 
 function cartback_tag($request) {
