6cded9433487df06e0ea720761c12026fe76853e
Mike Vercoelen <mike.vercoelen@gmail.com>
2021-01-06 12:54:40 +0800

    Added flagging logic

---
+++ cartback.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+  Plugin Name: 	CartBack
+  Plugin URI:		https://github.com/mikevercoelen/cartback
+  Description: 	Abandoned cart Mailchimp automation for WooCommerce
+  Version: 		0.1
+  Author: 		Benbodhi
+  Author URI: 	https://benbodhi.com
+  Text Domain: 	cartback
+  Domain Path:	/languages
+  License: 		GPLv2 or later
+  License URI:	http://www.gnu.org/licenses/gpl-2.0.html
+	Copyright 2021 and beyond | Benbodhi (email : wp@benbodhi.com)
+*/
+
+defined('ABSPATH') || exit;
+
+define('CARTBACK_PLUGIN_URL', plugin_dir_url(__FILE__));
+define('CARTBACK_PLUGIN_PATH', plugin_dir_path(__FILE__));
+
+include(CARTBACK_PLUGIN_PATH . 'admin/settings.php');
+include(CARTBACK_PLUGIN_PATH . 'vendor/MailChimp.php');
+include(CARTBACK_PLUGIN_PATH . 'includes/flag.php');
+// TODO: make me ...
+//  include(CARTBACK_PLUGIN_PATH . 'includes/unflag.php');
+
+add_action('wp_enqueue_scripts', 'cartback_enqueue_scripts_styles');
+
+function cartback_enqueue_scripts_styles() {
+  if (class_exists('woocommerce') && is_checkout()) {
+    wp_enqueue_script('ics-checkout', CARTBACK_PLUGIN_URL . '/public/cartback.min.js', array(), false, true);
+  }
+}
---
+++ includes/flag.php
@@ -0,0 +1,86 @@
+<?php
+
+defined('ABSPATH') || exit;
+
+$MC_API_KEY = get_option('cartback_setting', 'api_key');
+$MC_LIST_ID = get_option('cartback_setting', 'list_id');
+$MC_TAG = get_option('cartback_setting', 'mailchimp_tag');
+
+use \DrewM\MailChimp\MailChimp;
+
+$MailChimp = new MailChimp($MC_API_KEY);
+
+$MC_MEMBER_STATUS_NOT_FOUND = '404';
+$MC_MEMBER_STATUS_UNSUBSCRIBED = 'unsubscribed';
+$MC_TAG_STATUS_ACTIVE = 'active';
+
+function cartback_mc_get_member($subscriber_hash) {
+  global $MailChimp;
+  global $MC_LIST_ID;
+
+  return $MailChimp->get("lists/$MC_LIST_ID/members/$subscriber_hash");
+}
+
+function cartback_mc_subscribe($email) {
+  global $MailChimp;
+  global $MC_MEMBER_STATUS_UNSUBSCRIBED;
+  global $MC_LIST_ID;
+
+  return $MailChimp->post("lists/" . $MC_LIST_ID . "/members", [
+    'email_address' => $email,
+    'status' => $MC_MEMBER_STATUS_UNSUBSCRIBED
+  ]);
+}
+
+function cartback_mc_add_tag($subscriber_hash) {
+  global $MailChimp;
+  global $MC_TAG;
+  global $MC_TAG_STATUS_ACTIVE;
+  global $MC_LIST_ID;
+
+  return $MailChimp->post("lists/$MC_LIST_ID/members/$subscriber_hash/tags", [
+    'tags' => array(
+      [
+        'name' => $MC_TAG,
+        'status' => $MC_TAG_STATUS_ACTIVE
+      ]
+    )
+  ]);
+}
+
+function cartback_handle_mailchimp($email) {
+  global $MC_MEMBER_STATUS_NOT_FOUND;
+
+  $subscriber_hash = MailChimp::subscriberHash($email);
+  $member = cartback_mc_get_member($subscriber_hash);
+  $member_status = $member['status'];
+
+  if ($member_status === $MC_MEMBER_STATUS_NOT_FOUND) {
+    cartback_mc_subscribe($email);
+  }
+
+  cartback_mc_add_tag($subscriber_hash);
+}
+
+function cartback_get_email($request) {
+  if (is_user_logged_in()) {
+    $current_user = wp_get_current_user();
+    return $current_user->user_email;
+  }
+
+  $body = json_decode($request->get_body());
+  return $body->email;
+}
+
+function cartback_flag($request) {
+  $email = cartback_get_email($request);
+  cartback_handle_mailchimp($email);
+  return true;
+}
+
+add_action('rest_api_init', function () {
+  register_rest_route('cartback/v1', '/flag', array(
+    'methods' => 'POST',
+    'callback' => 'cartback_flag'
+  ));
+});
