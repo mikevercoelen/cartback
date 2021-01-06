53e5f52f1f0b4ae625be201066bd5b4b83c4dac1
Benbodhi <5193411+benbodhi@users.noreply.github.com>
2021-01-06 20:34:03 +1100

    uninstall and settings check
    Added new settings checkbox to "delete settigngs from database on uninstall", added uninstall code with check if the option is selected, wrapped the plugin functionality outside of settings page in a check if the settings have been filled in so the plugin doesn't do anything until settings are set.

--- cartback.php
+++ cartback.php
@@ -20,41 +20,48 @@ define('CARTBACK_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('CARTBACK_PLUGIN_PATH', plugin_dir_path(__FILE__));
 
 include(CARTBACK_PLUGIN_PATH . 'admin/settings.php');
-include(CARTBACK_PLUGIN_PATH . 'vendor/MailChimp.php');
-include(CARTBACK_PLUGIN_PATH . 'includes/tag.php');
 
-add_action('wp_enqueue_scripts', 'cartback_checkout_page');
-add_action( 'wp_enqueue_scripts', 'cartback_thankyou_page' );
+$CB_SETTINGS = get_option('cartback_setting');
+$MC_API_KEY = $CB_SETTINGS['api_key'];
+$MC_LIST_ID = $CB_SETTINGS['list_id'];
 
-use \DrewM\MailChimp\MailChimp;
+if ( !empty($MC_API_KEY && $MC_LIST_ID) ) {
+  include(CARTBACK_PLUGIN_PATH . 'vendor/MailChimp.php');
+  include(CARTBACK_PLUGIN_PATH . 'includes/tag.php');
 
-function cartback_checkout_page() {
-  if (class_exists('woocommerce') && is_checkout()) {
-    wp_enqueue_script('cartback-checkout', CARTBACK_PLUGIN_URL . '/public/cartback.min.js', array(), false, true);
-  }
-}
+  add_action('wp_enqueue_scripts', 'cartback_checkout_page');
+  add_action( 'wp_enqueue_scripts', 'cartback_thankyou_page' );
 
-function cartback_thankyou_page() {
-  if (class_exists('woocommerce') && is_order_received_page()) {
-    global $MC_TAG_STATUS_INACTIVE;
-    global $MC_TAG;
+  use \DrewM\MailChimp\MailChimp;
 
-    $order_id  = absint( get_query_var('order-received') );
-    $order = new WC_Order($order_id);
-    $email = $order->get_billing_email();
-
-    $subscriber_hash = MailChimp::subscriberHash($email);
-    $is_subscribed = cartback_mc_is_subscribed($email);
-
-    if (!$is_subscribed) {
-      return false;
+  function cartback_checkout_page() {
+    if (class_exists('woocommerce') && is_checkout()) {
+      wp_enqueue_script('cartback-checkout', CARTBACK_PLUGIN_URL . '/public/cartback.min.js', array(), false, true);
     }
+  }
 
-    cartback_mc_add_tags($subscriber_hash, array(
-      [
-        'name' => $MC_TAG,
-        'status' => $MC_TAG_STATUS_INACTIVE
-      ]
-    ));
+  function cartback_thankyou_page() {
+    if (class_exists('woocommerce') && is_order_received_page()) {
+      global $MC_TAG_STATUS_INACTIVE;
+      global $MC_TAG;
+
+      $order_id  = absint( get_query_var('order-received') );
+      $order = new WC_Order($order_id);
+      $email = $order->get_billing_email();
+
+      $subscriber_hash = MailChimp::subscriberHash($email);
+      $is_subscribed = cartback_mc_is_subscribed($email);
+
+      if (!$is_subscribed) {
+        return false;
+      }
+
+      cartback_mc_add_tags($subscriber_hash, array(
+        [
+          'name' => $MC_TAG,
+          'status' => $MC_TAG_STATUS_INACTIVE
+        ]
+      ));
+    }
   }
 }
--- uninstall.php
+++ uninstall.php
@@ -1,3 +1,23 @@
 <?php
 
-defined( 'ABSPATH' ) || exit;
+if (!defined('WP_UNINSTALL_PLUGIN')) exit;
+
+if (!get_option('uninstall_remove', false)) exit;
+
+delete_option('cartback_setting');
+
+if (is_multisite()) {
+    global $wpdb;
+    $blogs = $wpdb->get_results("SELECT blog_id FROM {$wpdb->blogs}", ARRAY_A);
+
+    if(!empty($blogs))
+    {
+        foreach($blogs as $blog) 
+        {
+        switch_to_blog($blog['blog_id']);
+            delete_option('cartback_setting');
+        }
+    }
+} else {
+    delete_option('cartback_setting');
+}
